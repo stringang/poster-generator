2ec22fd9c6d47cf36dd7271c4af4a74f04543df5
gang.liu <14845458+stringang@users.noreply.github.com>
2021-09-02 10:43:23 +0800

    chore: poster archive

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM node:14-alpine
 WORKDIR /poster
 # build node-canvas
 # https://github.com/node-gfx/node-canvas-prebuilt/issues/77
-RUN apk add --no-cache curl \
+RUN apk add --no-cache curl bash \
  && apk add --no-cache --virtual .build-deps build-base g++ \
  && apk add --no-cache --virtual .npm-deps cairo-dev libjpeg-turbo-dev pango-dev giflib-dev
 ADD . .
 RUN npm install && apk del .build-deps
 ENTRYPOINT []
-CMD ["sh"]
+CMD ["bash"]
 
--- scripts/poster.sh
+++ scripts/poster.sh
@@ -1,14 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 webhook=${WEBHOOK_ADDRESS}
 picture_address=${PICTURE_ADDRESS}
+host=${HOST}
 
 node /poster/src/index.js
+generator_path=/poster/poster.png
+pic_base64=$(base64 ${generator_path} | tr -d '\n')
+pic_md5=$(md5sum ${generator_path} | awk '{ print $1 }')
 
-pic_base64=$(base64 /poster/poster.png | tr -d '\n')
-pic_md5=$(md5sum /poster/poster.png | awk '{ print $1 }')
-
+# tencent wecom webhook
 echo '{ "msgtype": "image", "image": { "base64": "'$pic_base64'", "md5": "'$pic_md5'" } }' |
-  curl $webhook -X POST \
-    -H 'Content-Type: application/json' \
-    -d @- || exit 1
+curl -s $webhook -X POST \
+  -H 'Content-Type: application/json' \
+  -d @- || exit 1
+
+# archive to tencent COS
+if [ ${host} ]; then
+  year=$(date '+%Y')
+  month=$(date '+%m')
+  fileName=$(date '+%d')
+  curl -s --location --request POST "${host}" \
+    --form "file_data=@"${generator_path}"" \
+    --form 'fileExt="png"' \
+    --form "uploadPath="/poster/${year}/${month}/"" \
+    --form "fileName="${fileName}.png""
+fi
